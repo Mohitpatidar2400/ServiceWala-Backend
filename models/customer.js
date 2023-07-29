36edca14ee07f52a24772e25201588786d8588c9
Mohitpatidar2400 <mohit.patidar2400@gmail.com>
2023-07-29 14:41:12 +0530

    first commit

---
+++ models/customer.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+    select: false,
+  },
+  mobile_number:{
+    required: true,
+    type: Number,
+  },
+  address: {
+    required: true,
+    type: String
+  },
+  image: {
+    required: true,
+    type: String
+  },
+  latitude: {
+    type :Number
+  },
+  longitude: {
+    type :Number
+  },
+  
+ 
+});
+
+export const Customer = mongoose.model("Customer", schema);
